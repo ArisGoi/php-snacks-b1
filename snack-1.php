b4f5cb599bfe98f11772f1987c2c8ca24d9a1c33
ArisGoi <aris.goi97@gmail.com>
2021-10-19 14:10:54 +0200

    add snack-1

--- snack-1.php
+++ snack-1.php
@@ -52,3 +52,28 @@
 
 ?>
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>NBA Lasts Match</title>
+</head>
+<body>
+    <h1>NBA 6 Lasts Match</h1>
+    <ul>
+        <li><strong><?php echo $partite[0]["sCasa"] . " - " . $partite[0]["sOspite"]?></strong> | <?php echo $partite[0]["sCasaPunti"] . " - " . $partite[0]["sOspitePunti"]?></li>
+
+        <li><strong><?php echo $partite[1]["sCasa"] . " - " . $partite[1]["sOspite"]?></strong> | <?php echo $partite[1]["sCasaPunti"] . " - " . $partite[1]["sOspitePunti"]?></li>
+
+        <li><strong><?php echo $partite[2]["sCasa"] . " - " . $partite[2]["sOspite"]?></strong> | <?php echo $partite[2]["sCasaPunti"] . " - " . $partite[2]["sOspitePunti"]?></li>
+
+        <li><strong><?php echo $partite[3]["sCasa"] . " - " . $partite[3]["sOspite"]?></strong> | <?php echo $partite[3]["sCasaPunti"] . " - " . $partite[3]["sOspitePunti"]?></li>
+
+        <li><strong><?php echo $partite[4]["sCasa"] . " - " . $partite[4]["sOspite"]?></strong> | <?php echo $partite[4]["sCasaPunti"] . " - " . $partite[4]["sOspitePunti"]?></li>
+
+        <li><strong><?php echo $partite[5]["sCasa"] . " - " . $partite[5]["sOspite"]?></strong> | <?php echo $partite[5]["sCasaPunti"] . " - " . $partite[5]["sOspitePunti"]?></li>
+    </ul>
+</body>
+</html>
