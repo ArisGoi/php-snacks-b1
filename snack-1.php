b2ee0edbbb18b083a90528489e4594c255541f3d
ArisGoi <aris.goi97@gmail.com>
2021-10-19 14:01:53 +0200

    add array partite

--- snack-1.php
+++ snack-1.php
@@ -1,3 +1,54 @@
 <?php 
+    // Creiamo un array contenente le partite di basket di un’ipotetica tappa del calendario.
+    // Ogni array avrà una squadra di casa e una squadra ospite, punti fatti dalla squadra di casa e punti fatti dalla squadra ospite.
+    // Stampiamo a schermo tutte le partite con questo schema.
+
+    // Olimpia Milano - Cantù | 55-60
+
+    $partite= [
+        [
+            "date" => "16/10/21",
+            "sCasa" => "Warriors",
+            "sCasaPunti" => 119,
+            "sOspite" => "Trail Blazers",
+            "sOspitePunti" => 97
+        ],
+        [
+            "date" => "16/10/21",
+            "sCasa" => "Spurs",
+            "sCasaPunti" => 126,
+            "sOspite" => "Rockets",
+            "sOspitePunti" => 98
+        ],
+        [
+            "date" => "16/10/21",
+            "sCasa" => "Bulls",
+            "sCasaPunti" => 118,
+            "sOspite" => "Grizzlies",
+            "sOspitePunti" => 105
+        ],
+        [
+            "date" => "16/10/21",
+            "sCasa" => "Bucks",
+            "sCasaPunti" => 103,
+            "sOspite" => "Mavericks",
+            "sOspitePunti" => 114
+        ],
+        [
+            "date" => "16/10/21",
+            "sCasa" => "Knicks",
+            "sCasaPunti" => 115,
+            "sOspite" => "Wizards",
+            "sOspitePunti" => 113
+        ],
+        [
+            "date" => "16/10/21",
+            "sCasa" => "Pacers",
+            "sCasaPunti" => 94,
+            "sOspite" => "Cavaliers",
+            "sOspitePunti" => 110
+        ],
+    ];
 
 ?>
+
