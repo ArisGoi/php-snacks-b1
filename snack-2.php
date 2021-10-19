35d004e5cceec414c3eb03d8de1da76efec1057d
ArisGoi <aris.goi97@gmail.com>
2021-10-19 14:46:19 +0200

    add snack-2

--- snack-2.php
+++ snack-2.php
@@ -1,6 +1,5 @@
-<?php 
-    // Passare come parametri GET name, mail e age e verificare (cercando i metodi che non conosciamo nella documentazione) che name sia più lungo di 3 caratteri, che mail contenga un punto e una chiocciola e che age sia un numero. Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato”
-?>
+
+<!-- Passare come parametri GET name, mail e age e verificare (cercando i metodi che non conosciamo nella documentazione) che name sia più lungo di 3 caratteri, che mail contenga un punto e una chiocciola e che age sia un numero. Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato” -->
 
 <!DOCTYPE html>
 <html lang="en">
@@ -19,9 +18,24 @@
         <input type="text" name="mail" id="mail">
 
         <label for="age">Età:</label>
-        <input type="text" name="age" id="age">
+        <input type="number" name="age" id="age">
 
         <button>INVIA</button>
     </form>
+
+    <?php
+        $name = $_GET["name"];
+        $mail = $_GET["mail"];
+        $age = $_GET["age"];
+
+        if(strlen($name) > 3 && strpos($mail, "@") && strpos($mail, ".") && is_numeric($age)){
+            echo "<h2>Accesso Riuscito</h2>";
+        } elseif ($name == null && $mail == null && $age == null) {
+            echo "<p>Inserisci i dati di accesso qui sopra</p>";
+        } else {
+            echo "<h2>Accesso Negato</h2>";
+        }
+    ?>
+
 </body>
 </html>
